15f0146f05024c21df0346b2591525e1f6041a0c
DanielTM999 <danielmelo101203@gmail.com>
2026-08-04 13:43:03 -0300

    .

--- DependencyManager/src/main/java/dtm/dependencymanager/core/AbstractApplication.java
+++ DependencyManager/src/main/java/dtm/dependencymanager/core/AbstractApplication.java
@@ -12,7 +12,7 @@ import dtm.dependencymanager.exceptions.ApplicationStartupException;
 import dtm.dependencymanager.internal.Autoloader;
 
 public abstract class AbstractApplication extends Application {
-    protected void autoloader(DependencyContainerLaz dependencyContainer){
+    protected void autoloader(DependencyContainer dependencyContainer){
 
         try{
             ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
